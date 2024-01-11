358a3d3e7c56005325f5f282e2dd91700b41062c
Robson Coura <robson_coura@yahoo.com.br>
2024-01-11 15:55:07 -0300

    Anotações JPA para classe Chamado

--- helpdesk/src/main/java/com/robson/helpdesk/domain/Chamado.java
+++ helpdesk/src/main/java/com/robson/helpdesk/domain/Chamado.java
@@ -1,22 +1,44 @@
 package com.robson.helpdesk.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.robson.helpdesk.domain.enums.Prioridade;
 import com.robson.helpdesk.domain.enums.Status;
 
-public class Chamado {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Chamado implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	// Atributos - são as propriedades de um objeto
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataAbertura = LocalDate.now();
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento;
+
 	private Prioridade prioridade;
 	private Status status;
 	private String titulo;
 	private String observacoes;
+
+	@ManyToOne
+	@JoinColumn(name = "tecnico_id")
 	private Tecnico tecnico;
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
 
 	// Construtor sem argumentos
@@ -111,7 +133,8 @@ public class Chamado {
 	}
 
 	/*
-	 * HashCode - Fazer comparacao pelo seu valor dos seus atributos e nao pelo valor em memoria
+	 * HashCode - Fazer comparacao pelo seu valor dos seus atributos e nao pelo
+	 * valor em memoria
 	 */
 	@Override
 	public int hashCode() {
