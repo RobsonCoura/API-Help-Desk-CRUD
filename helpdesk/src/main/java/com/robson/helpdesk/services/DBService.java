3405f5d6274ea33fda95d0f2e03c8e33c16d861a
Robson Coura <robson_coura@yahoo.com.br>
2024-01-11 18:10:56 -0300

    Criando a classe DBService para a regra de negocio

---
+++ helpdesk/src/main/java/com/robson/helpdesk/services/DBService.java
@@ -0,0 +1,44 @@
+package com.robson.helpdesk.services;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.robson.helpdesk.domain.Chamado;
+import com.robson.helpdesk.domain.Cliente;
+import com.robson.helpdesk.domain.Tecnico;
+import com.robson.helpdesk.domain.enums.Perfil;
+import com.robson.helpdesk.domain.enums.Prioridade;
+import com.robson.helpdesk.domain.enums.Status;
+import com.robson.helpdesk.repositories.ChamadoRepository;
+import com.robson.helpdesk.repositories.ClienteRepository;
+import com.robson.helpdesk.repositories.TecnicoRepository;
+
+@Service
+public class DBService {
+
+	// Injecao de dependencias
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+
+	public void instanciaDB() {
+		// Criacao das instancias de Tecnico | Cliente | Chamado
+		Tecnico tec1 = new Tecnico(null, "Robson Dev", "63653230268", "robson@mail.com", "123");
+		tec1.addPerfil(Perfil.ADMIN);
+
+		Cliente cli1 = new Cliente(null, "Linus Torvalds", "80527954780", "torvalds@mail.com", "123");
+
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1,
+				cli1);
+
+		// Salvar as instancias no banco de dados
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(c1));
+	}
+}
