c9611a6e966bf1eb6b8f2507aa97ec0cabafa17f
Robson Coura <robson_coura@yahoo.com.br>
2024-04-16 21:26:24 -0300

    Implementação da classe ClienteDTO

---
+++ helpdesk/src/main/java/com/robson/helpdesk/domain/dtos/ClienteDTO.java
@@ -0,0 +1,105 @@
+package com.robson.helpdesk.domain.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.robson.helpdesk.domain.Cliente;
+import com.robson.helpdesk.domain.enums.Perfil;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ClienteDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    protected Integer id;
+    @NotNull(message = "Campo NOME é requerido")
+    protected String nome;
+    @NotNull(message = "Campo CPF é requerido")
+    protected String cpf;
+    @NotNull(message = "Campo EMAIL é requerido")
+    protected String email;
+    @NotNull(message = "Campo SENHA é requerido")
+    protected String senha;
+    protected Set<Integer> perfis = new HashSet<>();
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    protected LocalDate dataCriacao = LocalDate.now();
+
+    // Construtor sem argumentos
+    public ClienteDTO() {
+        super();
+        addPerfil(Perfil.CLIENTE);
+    }
+
+    // Construtor com argumentos
+    public ClienteDTO(Cliente obj) {
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.cpf = obj.getCpf();
+        this.email = obj.getEmail();
+        this.senha = obj.getSenha();
+        this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+        this.dataCriacao = obj.getDataCriacao();
+        addPerfil(Perfil.CLIENTE);
+    }
+
+    // Getters e Setters - Acessar e modificar os atributos
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Set<Perfil> getPerfis() {
+        return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+    }
+
+    public void addPerfil(Perfil perfil) {
+        this.perfis.add(perfil.getCodigo());
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+}
