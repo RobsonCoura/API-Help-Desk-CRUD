cc0d806a202b827bc38a57eb6fec850afe332313
Robson Coura <robson_coura@yahoo.com.br>
2024-05-04 13:14:44 -0300

    Implementando o método configure(HttpSecurity http)

---
+++ helpdesk/src/main/java/com/robson/helpdesk/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.robson.helpdesk.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //Método configure para proteger os endpoints contra vulnerabilidade
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+}
