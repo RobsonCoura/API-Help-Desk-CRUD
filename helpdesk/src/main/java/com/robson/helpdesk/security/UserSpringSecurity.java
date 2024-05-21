0440342d9734deeefb9ac49784a2927a9ca9621b
Robson Coura <robson_coura@yahoo.com.br>
2024-05-21 20:22:35 -0300

    Criando a classe UserSpringSecurity

---
+++ helpdesk/src/main/java/com/robson/helpdesk/security/UserSpringSecurity.java
@@ -0,0 +1,69 @@
+package com.robson.helpdesk.security;
+
+import com.robson.helpdesk.domain.enums.Perfil;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class UserSpringSecurity implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
+    //Atributos
+    private Integer id;
+    private String email;
+    private String senha;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserSpringSecurity(Integer id, String email, String senha, Set<Perfil> perfis) {
+        this.id = id;
+        this.email = email;
+        this.senha = senha;
+        this.authorities = perfis.stream().map(x -> new SimpleGrantedAuthority(x.getDescricao())).collect(Collectors.toSet());
+    }
+
+    //Métodos do contrato UserDetails
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
