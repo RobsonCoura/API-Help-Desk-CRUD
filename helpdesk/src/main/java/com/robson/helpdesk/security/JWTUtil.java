73762180098ef6de38cb4afd3de8014c1e6afdf6
Robson Coura <robson_coura@yahoo.com.br>
2024-05-21 21:28:28 -0300

    Criando uma classe JWTUtil

---
+++ helpdesk/src/main/java/com/robson/helpdesk/security/JWTUtil.java
@@ -0,0 +1,28 @@
+package com.robson.helpdesk.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+
+    //Atributos
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    //Método para gerar o token
+    public String generateToken(String email) {
+        return Jwts.builder() //Jwts é uma classe fornecida pela biblioteca JWT, o método builder() cria um novo builder para construir um token JWT.
+                .setSubject(email) // Define o "subject" (assunto) do token como sendo o email passado como argumento.
+                .setExpiration(new Date(System.currentTimeMillis() + expiration)) //Define a data de expiração do token.
+                .signWith(SignatureAlgorithm.HS512, secret.getBytes()) //Converte a chave secreta (secret) para um array de bytes.
+                .compact();
+    }
+}
