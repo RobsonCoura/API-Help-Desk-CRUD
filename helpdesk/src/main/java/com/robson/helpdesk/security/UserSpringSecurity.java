8b42c1b5eab9e82261232182d8a66d589a0d2eec
Robson Coura <robson_coura@yahoo.com.br>
2024-05-21 21:02:18 -0300

    Implementação de comentários na UserSpringSecurity

--- helpdesk/src/main/java/com/robson/helpdesk/security/UserSpringSecurity.java
+++ helpdesk/src/main/java/com/robson/helpdesk/security/UserSpringSecurity.java
@@ -19,6 +19,7 @@ public class UserSpringSecurity implements UserDetails {
     private String senha;
     private Collection<? extends GrantedAuthority> authorities;
 
+    // Define um construtor para inicializar os campos da classe. Recebe como parâmetros o id, senha, email e as autoridades do usuário.
     public UserSpringSecurity(Integer id, String email, String senha, Set<Perfil> perfis) {
         this.id = id;
         this.email = email;
@@ -26,7 +27,7 @@ public class UserSpringSecurity implements UserDetails {
         this.authorities = perfis.stream().map(x -> new SimpleGrantedAuthority(x.getDescricao())).collect(Collectors.toSet());
     }
 
-    //Métodos do contrato UserDetails
+    //Método que retorna a coleção de autoridades do usuário.
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return authorities;
@@ -36,31 +37,37 @@ public class UserSpringSecurity implements UserDetails {
         return id;
     }
 
+    //Método que retorna o senha do usuário
     @Override
     public String getPassword() {
         return senha;
     }
 
+    //Método que retorna o email do usuário
     @Override
     public String getUsername() {
         return email;
     }
 
+    //Método que indica se a conta não está expirada.
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
+    // Método que indica se a conta não está bloqueada.
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
+    // Método que indica se as credenciais não estão expiradas.
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
+    // Método que indica se a conta do usuário está ativa ou não.
     @Override
     public boolean isEnabled() {
         return true;
