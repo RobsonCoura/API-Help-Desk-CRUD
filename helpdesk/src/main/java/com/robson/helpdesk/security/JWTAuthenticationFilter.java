443fbc3a663b6d5f504413c8299b707b42aa7de1
Robson Coura <robson_coura@yahoo.com.br>
2024-06-07 21:27:10 -0300

    Implementando método Authentication atttemptAuthentication

--- helpdesk/src/main/java/com/robson/helpdesk/security/JWTAuthenticationFilter.java
+++ helpdesk/src/main/java/com/robson/helpdesk/security/JWTAuthenticationFilter.java
@@ -1,7 +1,16 @@
 package com.robson.helpdesk.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.robson.helpdesk.domain.dtos.CredenciaisDTO;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -15,4 +24,23 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         this.autheticationManager = autheticationManager;
         this.jwtUtil = jwtUtil;
     }
+
+    // Método que tenta autenticar o usuário com base nas credenciais fornecidas na requisição
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            // Lê as credenciais do corpo da requisição e mapeia para o objeto CredenciaisDTO
+            CredenciaisDTO creds = new ObjectMapper().readValue(request.getInputStream(), CredenciaisDTO.class);
+            // Cria um token de autenticação com o email e senha extraídos das credenciais
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getSenha(), new ArrayList<>());
+            // Autentica o token utilizando o authenticationManager
+            Authentication authentication = autheticationManager.authenticate(authenticationToken);
+            // Retorna o objeto de autenticação se bem-sucedido
+            return authentication;
+        } catch (Exception e) {
+            // Lança uma RuntimeException em caso de falha
+            throw new RuntimeException(e);
+        }
+    }
+    
 }
