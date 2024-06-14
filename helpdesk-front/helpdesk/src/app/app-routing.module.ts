9898bd84359f04510412a4b234df115637a9c2cb
RobsonCoura <robson_coura@yahoo.com.br>
2024-06-14 18:29:24 -0300

    Rota para componente NAV

--- helpdesk-front/helpdesk/src/app/app-routing.module.ts
+++ helpdesk-front/helpdesk/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', // Define o caminho da rota como vazio, indicando a rota raiz.
+    component: NavComponent  // Associa o componente NavComponent a este caminho da rota.
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
