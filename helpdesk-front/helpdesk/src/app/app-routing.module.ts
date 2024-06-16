e38928b90c915771849dd964411a1d6f3f7184bc
RobsonCoura <robson_coura@yahoo.com.br>
2024-06-16 20:07:08 -0300

    Rota para TECNICO-LIST criado

--- helpdesk-front/helpdesk/src/app/app-routing.module.ts
+++ helpdesk-front/helpdesk/src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 const routes: Routes = [
   {
     path: '', // Define o caminho da rota como vazio, indicando a rota raiz.
     // Associa o componente NavComponent a este caminho da rota.
     component: NavComponent, children: [
-      { path: 'home', component: HomeComponent } // Rota filha do componente NAVE
+      { path: 'home', component: HomeComponent }, // Rota filha do componente NAVE
+      { path: 'tecnicos', component: TecnicoListComponent}
     ]
   }
 ];
