209488da1b6046ef0017994f8479d37660324940
RobsonCoura <robson_coura@yahoo.com.br>
2024-06-16 19:22:47 -0300

    Rota para componente HOME

--- helpdesk-front/helpdesk/src/app/app-routing.module.ts
+++ helpdesk-front/helpdesk/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {
     path: '', // Define o caminho da rota como vazio, indicando a rota raiz.
-    component: NavComponent  // Associa o componente NavComponent a este caminho da rota.
+    // Associa o componente NavComponent a este caminho da rota.
+    component: NavComponent, children: [
+      { path: 'home', component: HomeComponent } // Rota filha do componente NAVE
+    ]
   }
 ];
 
