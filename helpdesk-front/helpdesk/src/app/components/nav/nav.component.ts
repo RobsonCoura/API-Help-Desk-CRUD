b558853aca17b38037d09866bb048d6a1ca69c15
RobsonCoura <robson_coura@yahoo.com.br>
2024-06-17 12:10:54 -0300

    Implementando linhas de comentários Eficientes em Componente

--- helpdesk-front/helpdesk/src/app/components/nav/nav.component.ts
+++ helpdesk-front/helpdesk/src/app/components/nav/nav.component.ts
@@ -1,17 +1,21 @@
+// Importações necessárias do Angular
 import { Component, OnInit } from '@angular/core';
-import { Route, Router } from '@angular/router';
+import { Router } from '@angular/router'; // Importa Router para navegação
 
+// Componente Angular para a barra de navegação
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  selector: 'app-nav', // Seletor do componente
+  templateUrl: './nav.component.html', // Template HTML associado ao componente
+  styleUrls: ['./nav.component.css'] // Estilos CSS associados ao componente
 })
 export class NavComponent implements OnInit {
 
+  // Construtor do componente, injeta o serviço Router para navegação
   constructor(private router: Router) { }
 
+  // Método executado quando o componente é inicializado
   ngOnInit(): void {
-    this.router.navigate(['home'])
+    this.router.navigate(['home']); // Navega para a rota 'home' ao inicializar
   }
 
 }
