865d5354821075e17f90fbf6779cc70860261bea
RobsonCoura <robson_coura@yahoo.com.br>
2024-06-17 12:17:19 -0300

    Implementando linhas de comentários Eficientes em TECNICO

--- helpdesk-front/helpdesk/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ helpdesk-front/helpdesk/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,15 +1,18 @@
+// Importações de módulos e classes do Angular e do Material
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-import { Tecnico } from 'src/app/models/tecnico';
+import { Tecnico } from 'src/app/models/tecnico'; // Importa o modelo Tecnico
 
+// Decorador @Component para o componente TecnicoListComponent
 @Component({
   selector: 'app-tecnico-list',
-  templateUrl: './tecnico-list.component.html',
-  styleUrls: ['./tecnico-list.component.css']
+  templateUrl: './tecnico-list.component.html', // Arquivo HTML de template
+  styleUrls: ['./tecnico-list.component.css'] // Arquivo de estilos CSS
 })
 export class TecnicoListComponent implements OnInit {
 
+  // Dados de exemplo para a tabela
   ELEMENT_DATA: Tecnico[] = [
     {
       id: 1,
@@ -20,9 +23,12 @@ export class TecnicoListComponent implements OnInit {
       perfis: ['0'],
       dataCriacao: '15/08/2022'
     }
-  ]
+  ];
 
+  // Colunas exibidas na tabela
   displayedColumns: string[] = ['id', 'nome', 'cpf', 'email', 'acoes'];
+
+  // Fonte de dados da tabela do tipo MatTableDataSource com os dados iniciais ELEMENT_DATA
   dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
 
   constructor() { }
@@ -30,8 +36,10 @@ export class TecnicoListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // ViewChild para obter a instância do paginador MatPaginator da visualização
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
+  // Método ngAfterViewInit para configurar o paginador após a visualização do componente
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
   }
