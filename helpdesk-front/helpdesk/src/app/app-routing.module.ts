4dac61a00b0834a798ac5dff182e713bcc546930
RobsonCoura <robson_coura@yahoo.com.br>
2024-06-17 13:35:49 -0300

    Implementando autenticação do Login

--- helpdesk-front/helpdesk/src/app/app-routing.module.ts
+++ helpdesk-front/helpdesk/src/app/app-routing.module.ts
@@ -4,21 +4,26 @@ import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './auth/auth.guard'; // Importa o guarda de autenticação
 
+// Definição das rotas da aplicação
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent }, // Rota para o componente de login
+
+  // Rota principal, associada ao componente NavComponent e protegida pelo AuthGuard
   {
     path: '', // Define o caminho da rota como vazio, indicando a rota raiz.
-    // Associa o componente NavComponent a este caminho da rota.
-    component: NavComponent, children: [
-      { path: 'home', component: HomeComponent }, // Rota filha do componente NAVE
-      { path: 'tecnicos', component: TecnicoListComponent }
+    component: NavComponent, // Associa o componente NavComponent a este caminho da rota.
+    canActivate: [AuthGuard], // Protege a rota usando o AuthGuard
+    children: [
+      { path: 'home', component: HomeComponent }, // Rota filha para o componente Home
+      { path: 'tecnicos', component: TecnicoListComponent } // Rota filha para o componente TecnicoListComponent
     ]
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)], // Configuração do módulo de roteamento com as rotas definidas
+  exports: [RouterModule] // Exporta o módulo de roteamento para ser utilizado na aplicação
 })
 export class AppRoutingModule { }
--- helpdesk-front/helpdesk/src/app/components/login/login.component.ts
+++ helpdesk-front/helpdesk/src/app/components/login/login.component.ts
@@ -1,10 +1,12 @@
-// Importações e configurações de módulos
+// Importações de módulos necessários para o componente
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { Toast, ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Credenciais } from 'src/app/models/credenciais';
+import { AuthService } from 'src/app/services/auth.service';
 
-// Definição do componente com seletor, template e estilos
+// Definição do componente Angular
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,31 +14,44 @@ import { Credenciais } from 'src/app/models/credenciais';
 })
 export class LoginComponent implements OnInit {
 
-  // Inicialização das credenciais
+  // Inicialização das credenciais do usuário
   creds: Credenciais = {
     email: '',
     senha: ''
   };
 
-  // Definição dos controles de formulário com validações
+  // Controle de formulário para o campo de email com validação de email
   email = new FormControl(null, Validators.email);
+  // Controle de formulário para o campo de senha com validação de tamanho mínimo
   senha = new FormControl(null, Validators.minLength(3));
 
-  // Injeta o serviço ToastrService para notificações
-  constructor(private toast: ToastrService) { }
+  // Injeção de serviços necessários no componente
+  constructor(
+    private toast: ToastrService,
+    private service: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
-    // Inicialização adicional, se necessário
+    // Método do ciclo de vida OnInit para inicializações adicionais, se necessário
   }
 
-  // Método para realizar login
+  // Método para realizar o login do usuário
   logar() {
-    // Exibe uma mensagem de erro e limpa a senha
-    this.toast.error('Usuário e/ou senha inválidos!', 'Login');
-    this.creds.senha = '';
+    this.service.authenticate(this.creds).subscribe(
+      resposta => {
+        // Caso o login seja bem-sucedido, realiza o processamento
+        this.service.successfulLogin(resposta.headers.get('Authorization').substring(7));
+        this.router.navigate(['']);  // Navega para a rota principal após o login
+      },
+      () => {
+        // Em caso de erro no login, exibe uma mensagem de erro ao usuário
+        this.toast.error('Usuário e/ou senha inválidos');
+      }
+    );
   }
 
-  // Valida se os campos de email e senha são válidos
+  // Verifica se os campos de email e senha são válidos para habilitar o botão de login
   validaCampos(): boolean {
     return this.email.valid && this.senha.valid;
   }
