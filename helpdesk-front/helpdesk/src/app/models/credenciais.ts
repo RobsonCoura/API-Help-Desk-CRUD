344c8f0af9cb016bd82876bce97923547b352a70
RobsonCoura <robson_coura@yahoo.com.br>
2024-06-17 12:19:51 -0300

    Implementando linhas de comentários em Credenciais e Tecnico

--- helpdesk-front/helpdesk/src/app/models/credenciais.ts
+++ helpdesk-front/helpdesk/src/app/models/credenciais.ts
@@ -1,4 +1,5 @@
+// Interface Credenciais para representar informações de login
 export interface Credenciais {
-    email: string;
-    senha: string;
+    email: string; // Campo para o endereço de email
+    senha: string; // Campo para a senha
 }
