c9d54d9959da5c48e7f82765a0951413287be613
RobsonCoura <robson_coura@yahoo.com.br>
2024-06-18 13:47:30 -0300

    Rota para componente TECNICO-CREATE

--- helpdesk-front/helpdesk/src/app/app-routing.module.ts
+++ helpdesk-front/helpdesk/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './components/home/home.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './auth/auth.guard'; // Importa o guarda de autenticação
+import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 
 // Definição das rotas da aplicação
 const routes: Routes = [
@@ -17,7 +18,8 @@ const routes: Routes = [
     canActivate: [AuthGuard], // Protege a rota usando o AuthGuard
     children: [
       { path: 'home', component: HomeComponent }, // Rota filha para o componente Home
-      { path: 'tecnicos', component: TecnicoListComponent } // Rota filha para o componente TecnicoListComponent
+      { path: 'tecnicos', component: TecnicoListComponent }, // Rota filha para o componente TecnicoListComponent
+      { path: 'tecnicos/create', component: TecnicoCreateComponent } // Rota filha para o componente TecnicoListComponent
     ]
   }
 ];
