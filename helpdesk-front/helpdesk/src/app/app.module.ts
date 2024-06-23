3ca0eb1d1eb5bcf29e785e772b5177b37ef78f7b
RobsonCoura <robson_coura@yahoo.com.br>
2024-06-23 18:27:05 -0300

    Componente CHAMADO-LIST criado

--- helpdesk-front/helpdesk/src/app/app.module.ts
+++ helpdesk-front/helpdesk/src/app/app.module.ts
@@ -43,6 +43,7 @@ import { ClienteCreateComponent } from './components/cliente/cliente-create/clie
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
 import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
+import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 
 
 @NgModule({
@@ -60,6 +61,7 @@ import { ClienteListComponent } from './components/cliente/cliente-list/cliente-
     ClienteCreateComponent,
     ClienteUpdateComponent,
     ClienteDeleteComponent,
+    ChamadoListComponent,
   ],
   imports: [
     BrowserModule,
